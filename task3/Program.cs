7696b79e00469f29629dc442705bbfd698f40674
KatushaKononova56 <83833827+KatushaKononova56@users.noreply.github.com>
2022-11-04 16:06:59 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write_cyb();
+        }
+
+        static void Write_cyb(){
+            Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int num_cub=1;
+            while(num_cub!=num){
+                Console.Write(Math.Pow(num_cub,3) + ", ");
+                num_cub++;
+            }
+            Console.Write(Math.Pow(num,3));
+        }
+    }
+}
